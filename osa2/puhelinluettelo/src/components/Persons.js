23794b159c0e05ab8605c83a410e02e6ccbd7a5f
areee <10089872+areee@users.noreply.github.com>
2020-07-21 19:48:41 +0300

    Persons (for 2.17): removed unnecessary code

--- osa2/puhelinluettelo/src/components/Persons.js
+++ osa2/puhelinluettelo/src/components/Persons.js
@@ -6,14 +6,4 @@ const Persons = ({ person, deleteSelectedPerson }) => (
     </p>
 )
 
-// const Persons = ({ filteredPersons, deleteSelectedPerson }) => (
-//     <div>
-//         {filteredPersons.map((person, i) =>
-//             <p key={i}>
-//                 {person.name} {person.number} <button onClick={deleteSelectedPerson}>delete</button>
-//             </p>
-//         )}
-//     </div>
-// )
-
 export default Persons
